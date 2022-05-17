254d88f02b2d3976fe6d344311dff64eb4d6016c
Marco Vela <mvelar@uni.pe>
2022-05-17 18:41:28 -0500

    change name in metrics to test_loss

--- scripts/training/train.jl
+++ scripts/training/train.jl
@@ -191,7 +191,7 @@ for epoch in 1:args[:epochs]
   @info "Testing..." epoch
   metrics_dict, test_time = CUDA.@timed metrics_single_epoch(model, metrics, ((device(X), y) for (X,y) in test_data))
   Flux.reset!(model)
-  metrics_dict[:loss] = metrics_dict[loss_name]
+  metrics_dict[:test_loss] = metrics_dict[loss_name]
   push!(epoch_losses, metrics_dict[loss_name])
   Base.with_logger(logger) do
     @info "EPOCH_TEST" epoch exec_time=test_time metrics_dict...
