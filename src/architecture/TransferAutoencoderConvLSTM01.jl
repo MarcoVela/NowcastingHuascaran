c7921f6a557beed97bf6dd73e1f35ff3e474a45b
Marco Vela <mvelar@uni.pe>
2022-05-17 23:44:38 -0500

    transfer learning with custom hidden channel

--- src/architecture/TransferAutoencoderConvLSTM01.jl
+++ src/architecture/TransferAutoencoderConvLSTM01.jl
@@ -8,17 +8,18 @@ function determine_channels(c::Conv)
   size(c.weight, ndims(c))
 end
 
-function build_model(; out, device, base_model)
+function build_model(; out, device, base_model, ch="same")
   @assert (length(base_model) == 2) "Base model should only have 2 elements"
   encoder, decoder = base_model
-  ch = determine_channels(encoder[end]) # Asumiendo una capa conv o BatchNorm
+  _ch = determine_channels(encoder[end]) # Asumiendo una capa conv o BatchNorm
+  hidden_channels = ch == "same" ? _ch : ch
   temporal_encoder_decoder = Chain(
     KeepLast(
-      ConvLSTM2D((8, 8), (3, 3),  ch => ch, pad=SamePad()),
+      ConvLSTM2D((8, 8), (3, 3),  _ch => hidden_channels, pad=SamePad()),
     ),
     RepeatInput(
       out,
-      ConvLSTM2D((8, 8), (3, 3), ch => ch, pad=SamePad()),
+      ConvLSTM2D((8, 8), (3, 3), hidden_channels => _ch, pad=SamePad()),
     ),
   )
   _model = Chain(
