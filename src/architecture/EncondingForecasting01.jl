b31c3ccf152dba40539b556d816a7d8c3dd46958
Marco Vela <mvelar@uni.pe>
2022-05-20 15:33:31 -0500

    encoding forecaster
    model described in https://arxiv.org/abs/1506.04214

---
+++ src/architecture/EncondingForecasting01.jl
@@ -0,0 +1,25 @@
+using Flux
+using Flux: params
+using DrWatson
+
+include(srcdir("layers", "TimeDistributed.jl"))
+include(srcdir("layers", "KeepLast.jl"))
+include(srcdir("layers", "ConvLSTM2Dv2.jl"))
+
+function build_model(; out, device)
+  _model = Chain(
+    KeepLast(
+      ConvLSTM2Dv2((64, 64), (5, 5), (5, 5), 1=>256, pad=SamePad()),
+    ),
+    RepeatInput(
+      out,
+      ConvLSTM2Dv2((64, 64), (5, 5), (5, 5), 256=>256, pad=SamePad()),
+    ),
+    TimeDistributed(
+      Conv((1,1), 256=>1, pad=SamePad(), bias=false)
+    )
+  )
+  model = device(_model)
+  model, params(model)
+end
+
---
+++ src/layers/KeepLast.jl
@@ -0,0 +1,32 @@
+using Flux
+struct KeepLast{N, M<:Flux.Recur}
+  n::N
+  m::M
+end
+
+function KeepLast(m)
+  KeepLast(1, m)
+end
+
+function (k::KeepLast)(x::AbstractArray{T, N}) where {T, N}
+  before_dims = ntuple(_ -> :, N-1)
+  n2 = size(x, N)
+  discarted = ifelse(n2 - k.n < 0, 0, n2 - k.n)
+  k.m(view(x, before_dims..., 1:discarted))
+  k.m(view(x, before_dims..., discarted+1:n2))
+end
+
+Flux.@functor KeepLast (m,)
+
+Base.show(io::IO, k::KeepLast) = print(io,"KeepLast(", k.m, ")")# Flux._big_show(io, m)
+
+function Base.show(io::IO, ::MIME"text/plain", x::KeepLast)
+  if get(io, :typeinfo, nothing) === nothing  # e.g. top level in REPL
+    Flux._big_show(io, x)
+  elseif !get(io, :compact, false)  # e.g. printed inside a Vector, but not a Matrix
+    Flux._layer_show(io, x)
+  else
+    show(io, x)
+  end
+end
+
