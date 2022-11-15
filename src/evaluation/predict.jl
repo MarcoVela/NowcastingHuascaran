c84871490432d06245415550297c09094fda9e9e
Marco Vela <mvelar@uni.pe>
2022-11-14 19:13:36 -0500

    wip: grid

--- src/evaluation/predict.jl
+++ src/evaluation/predict.jl
@@ -22,15 +22,15 @@ end
 
 using Base.Iterators
 
-function get_new_axis(dataset; dimensions, steps=(1,1,1))
+function get_new_axis(dataset; dimensions, steps)
   axs = axes(dataset)
   for i in 1:3
     d = dims(dataset, i).val
     s = dimensions[i]
-    if (length(d) - s - 1) % steps[i] !== 0 
+    if (length(d) - s) % steps[i] !== 0 
       d_size = length(d)
       ax = axs[i]
-      last_ax = last((first(ax):steps[i]:(last(ax)-s)))+s
+      last_ax = last((first(ax):steps[i]:(last(ax)-s)))+s-1
       @warn "DimensionMismatch: Dataset $(d.metadata["standard_name"]) has dimensions $(d_size) but patch size is $(s) and step size is $(steps[i]). Reshaping to $(last_ax)."
       axs = Base.setindex(axs, range(first(ax), last_ax; step=step(ax)), i)
     end
@@ -38,8 +38,8 @@ function get_new_axis(dataset; dimensions, steps=(1,1,1))
   axs
 end
 
-function generate_grids(dataset, axs)
-  starts = ntuple(i -> range(first(axs[i]), last(axs[i]) - dimensions[i]; step=steps[i]), Val(3))
+function generate_grids(dataset, axs; dimensions, steps)
+  starts = ntuple(i -> range(first(axs[i]), last(axs[i]); step=steps[i]), Val(3))
   (dataset[ntuple(i -> idx[i]:(idx[i]+dimensions[i]-1), Val(3))...] for idx in Iterators.product(starts...))
 end
 
@@ -62,6 +62,18 @@ function predict_climarrs(model, climarrs; device_in, device_out)
 end
 
 # Unir arrays en uno solo
+# TODO: Refactorizar en una implementación más limpia sin tantas allocations
 function join_climarrs(climarrs)
+  lon, lat, tim = size(climarrs)
+  new_climarrs = collect((cat(climarrs[i, j, :]...; dims=3) for (i,j) in Iterators.product(1:lon, 1:lat)))
+  new_climarr = collect((cat(new_climarrs[i, :]...; dims=2) for i in 1:lon))
+  cat(new_climarr...; dims=1)
+end
+
+function generate_mosaic(folder, T₀, T₁; compression=1, N, S, E, W, device_in, device_out, dimensions, steps)
+  dataset = load_dataset(folder, T₀, T₁; compression, N, S, E, W)
+  axs = get_new_axis(dataset; dimensions, steps)
+  grids = collect(generate_grids(dataset, axs; dimensions, steps))
   
+
 end
